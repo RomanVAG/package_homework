09343f91a32a9ec4059039a200e53a7d13a951b0
Roman <vehsylamor@gmail.com>
2025-06-13 22:54:11 +0300

    add new file mask.py modified widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,8 @@
+from src.mask import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(account_info: str) -> str:
-    """
-    Маскирует номер карты или счета в переданной строке.
-
-    Параметры:
-        account_info: строка формата "Visa Platinum 7000792289606361" или "Счет 73654108430135874305"
-
-    Возвращает:
-        Строку с замаскированным номером (карта: XXXX XX** **** XXXX, счет: **XXXX)
-    """
+    """Маскирует номер карты или счета в переданной строке."""
     # Разделяем строку на части
     parts = account_info.split()
 
@@ -26,18 +21,6 @@ def mask_account_card(account_info: str) -> str:
         masked_number = get_mask_card_number(number)
 
     return f"{name} {masked_number}"
-
-
-def get_mask_card_number(card_number: str | int) -> str:
-    """Маскирует номер банковской карты (цифры с 7 по 12)"""
-    card_str = str(card_number)
-    return f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
-
-
-def get_mask_account(account: str | int) -> str:
-    """Маскирует номер банковского счёта (первые 12 цифр)"""
-    account_str = str(account)
-    return f"**{account_str[-4:]}"
 
 
 def get_date(date_str: str) -> str:
