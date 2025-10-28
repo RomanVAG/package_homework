058dab5f2dbba76fbd891af0340ed3efb97413df
Roman <vehsylamor@gmail.com>
2025-10-28 12:28:57 +0300

    modify func log

--- src/decorators.py
+++ src/decorators.py
@@ -13,20 +13,20 @@ def log(filename: Optional[str] = None) -> Callable:
     """
     def decorator_func(func: Any) -> Any:
         def wrapper(*args: tuple[Any], **kwargs: dict[str, Any]) -> Any | None:
-            start_time = datetime.now().replace(microsecond=0)  # время начала вызова функции
+            start_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")  # время начала вызова функции
             try:
                 # Рискованный код
-                func(*args, **kwargs)  # Вызов оригинальной функции
+                result = func(*args, **kwargs)  # Вызов оригинальной функции
                 if filename:   # Если filename задан, логи записываются в указанный файл
-                    with open("mylog.txt", "a", encoding='utf-8') as _:  # Логирование информации об ошибке
-                        _.write(f"{start_time} {func.__name__} ok \n")
+                    with open(filename, "a", encoding='utf-8') as _:  # Логирование информации об ошибке
+                        _.write(f"{start_time} {func.__name__} ok: {result} \n")
                 else: # Если файл не задан, логи выводятся в консоль
-                    print(f"{start_time} {func.__name__} ok \n")
-                return func # возврат оригинальной функции
+                    print(f"{start_time} {func.__name__} ok: {result} \n")
+                return result # возврат оригинальной функции
 
             except Exception as e:  # Обработка ошибок
                 if filename: # Если filename задан, логи записываются в указанный файл
-                    with open("mylog.txt", "a", encoding='utf-8') as _:  # Логирование информацию об ошибке
+                    with open(filename, "a", encoding='utf-8') as _:  # Логирование информацию об ошибке
                         _.write(f"{start_time} {func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs} \n")
                 else: # Если файл не задан, логи выводятся в консоль
                     print(f"{start_time} {func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs} \n")
@@ -41,7 +41,7 @@ def my_function(x, y):
     return x + y
 
 
-my_function("7", 6)
+my_function(7, 6)
 # # Вывод в консоль:
 # # 2024-01-15 14:30:25 - my_function error: тип ошибки. Inputs: (1, 2), {}
-# # 2024-01-15 14:30:25 - my_function ok
+# # 2024-01-15 14:30:25 k- my_function o
