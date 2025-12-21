a21244451a7c639d39aa24184260f7777b249a43
Roman <vehsylamor@gmail.com>
2025-12-21 20:40:45 +0300

    modify func load_transactions_from_json

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,14 @@
-def load_transactions_from_json():
-    pass
+import json
+import os
+
+def load_transactions_from_json(filepath):
+    with open(filepath, encoding= "UTF-8") as file:
+        data = json.load(file)
+    print(data)
+
+# Подняться на один уровень вверх, затем зайти в data
+filepath = os.path.join("..", "data", "operations.json")
+# Результат: "..\data\operations.json"
+
+load_transactions_from_json(filepath)
+
