8773e61f26f1482024cb51cecede264f0acd5414
Roman <vehsylamor@gmail.com>
2025-12-23 21:50:19 +0300

    add exceptions in func load_transactions_from_json

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,25 @@
 import json
 import os
 
+
 def load_transactions_from_json(filepath):
-    with open(filepath, encoding= "UTF-8") as file:
-        data = json.load(file)
-    print(data)
+    try:
+        with open(filepath, encoding="UTF-8") as file:
+            data = json.load(file)
+        print(data)
+    #   return data
+    except json.JSONDecodeError:  # файл пустой
+        print("File is empty")
+        return []
+    except ValueError:  # содержит не список
+        print("File contains't  no list")
+        return []
+    except FileNotFoundError: # файл не найден
+        print("File not found")
+        return []
 
 # Подняться на один уровень вверх, затем зайти в data
 filepath = os.path.join("..", "data", "operations.json")
 # Результат: "..\data\operations.json"
 
 load_transactions_from_json(filepath)
-
