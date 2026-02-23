9afbb04f4d53f49506ab12b87a0d0f0c268e00ec
Roman <vehsylamor@gmail.com>
2026-02-23 20:53:23 +0300

    add annotations

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import os
 
 
 def load_transactions_from_json(filepath):
+    """Функция чтения JSON-файла возвращает список словарей с данными о финансовых транзакциях.
+     Функция принимает на вход путь к JSON файлу в качестве аргумента."""
     try:
         with open(filepath, encoding="UTF-8") as file:
             data = json.load(file)
@@ -10,16 +12,16 @@ def load_transactions_from_json(filepath):
     #   return data
     except json.JSONDecodeError:  # файл пустой
         print("File is empty")
-        return []
+        return [] # возвращается пустой список.
     except ValueError:  # содержит не список
         print("File contains't  no list")
-        return []
+        return [] # возвращается пустой список.
     except FileNotFoundError: # файл не найден
         print("File not found")
-        return []
+        return [] # возвращается пустой список.
 
 # Подняться на один уровень вверх, затем зайти в data
-filepath = os.path.join("..", "data", "operations.json")
+filepath = os.path.join("..", "data", "operations.json") # путь к файлу JSON
 # Результат: "..\data\operations.json"
 
 load_transactions_from_json(filepath)
