81e34195632b0d726418d5be9e340ea805ca1a35
Roman <vehsylamor@gmail.com>
2026-02-26 23:07:42 +0300

    add GET request

--- src/external_api.py
+++ src/external_api.py
@@ -1,12 +1,51 @@
-import json
 import os
+from dotenv import load_dotenv
+import requests
+from src.utils import load_transactions_from_json
 
-def conversion_from_USD_and_EUR_to_RUB(dict: data) -> float:
+# Загрузка переменных из .env-файла
+load_dotenv()
+
+# Получение значения переменной API_KEY из .env-файла
+API_KEY = os.getenv('API_KEY')
+
+# Задаем адрес сайта, к которому хотим обратиться
+url = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=amount"
+
+payload = {}
+headers= {
+  "apikey": API_KEY
+}
+# Выполняем GET-запрос к сайту и сохраняем ответ в переменную response
+response = requests.request("GET", url, headers=headers, data = payload)
+# Получаем статус-код из ответа и выводим его на экран
+status_code = response.status_code
+result = response.text
+print(f"Статус код: {status_code}")
+
+# Проверяем, равен ли статус-код 200, то есть чтобы запрос был успешным
+if status_code == 200:
+    # Выводим содержимое сайта на экран
+    content = response.text
+    print(f"Содержимое сайта:\n{content}")
+else:
+    # Выводим сообщение об ошибке
+    print(f"Запрос не был успешным. Возможная причина: {response.reason}")
+
+def conversion_from_USD_and_EUR_to_RUB(id, amount) -> float:
     """
-    Функция конвертации валюты из USD и EUR в рубли принимает на вход словарь с данными о транзакции.
-    Функция конвертации валюты из USD и EUR в рубли возвращает сумму транзакции (ключ amount)
-    в рублях, тип данных float. Если транзакция была в USD или EUR, происходит обращение к внешнему
-    API для получения текущего курса валют и конвертации суммы операции в рубли.
+    Функция, которая принимает на вход транзакцию (id), сумму (amount) и возвращает сумму транзакции
+    (amount) в рублях, тип данных — float. Если транзакция была в USD или EUR, происходит обращение
+    к внешнему API для получения текущего курса валют и конвертации суммы операции в рубли.
     """
     pass
 
+
+"""
+# Подняться на один уровень вверх, затем зайти в data
+filepath = os.path.join("..", "data", "operations.json") # путь к файлу JSON
+# Результат: "..\data\operations.json"
+
+data = load_transactions_from_json(filepath)
+data
+"""
